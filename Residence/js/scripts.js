8c1327b2758b317431c4fc81f92ce5eb5d3dda10
Vanvrs <107437663+Vanvrs@users.noreply.github.com>
2023-09-20 09:53:27 -0300

    Projeto Bootstrap
    Projeto realizado via bootstrap.

---
+++ Residence/js/scripts.js
@@ -0,0 +1,7 @@
+//console.log("teste")
+/* alert("Olá, Devs UniNassau")
+window.alert("Está é minha primeira mensagem!!")
+window.confirm("Está gostando de JavaScript?")
+prompt("Qual é o seu nome?")
+document.write("estou escrevendo no corpo da página")
+ */
